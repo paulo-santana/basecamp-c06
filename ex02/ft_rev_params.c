149fe69c43667c0074350b00faf427cde79ff283
Paulo Santana <paulo.santana.r@gmail.com>
2021-04-14 22:33:04 -0300

    rev params

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int		ft_strlen(char *str)
+{
+	int	length;
+
+	length = 0;
+	while (*str++)
+		length++;
+	return length;
+}
+
+void	ft_putstr(char *str)
+{
+	unsigned int	length;
+
+	length = ft_strlen(str);
+	write(1, str, length);
+}
+
+int main(int argc, char *argv[])
+{
+	while (--argc)
+	{
+		ft_putstr(argv[argc]);
+		ft_putstr("\n");
+	}
+}
